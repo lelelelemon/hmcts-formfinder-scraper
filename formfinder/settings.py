4d99a2fcb1477b16c06994ad2e97ce9d3fe17aee
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2015-09-07 17:37:54 +0100

    Added local file feed output

--- formfinder/settings.py
+++ formfinder/settings.py
@@ -2,3 +2,5 @@ SPIDER_MODULES = ['formfinder.spiders']
 NEWSPIDER_MODULE = 'formfinder.spiders'
 DEFAULT_ITEM_CLASS = 'formfinder.items.Form'
 #ITEM_PIPELINES = {'formfinder.'}
+FEED_FORMAT = 'jsonlines'
+FEED_URI = 'file://' + os.path.join(HERE, '../output.jsonl')
